f9c49561d4806dd10cab8cf6e5ad6488335d22de
phuc-loc <phuclocnguyen0701@gmail.com>
2022-01-15 16:15:13 +0700

    react route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import Main from './components/MainComponent';
 import './App.css';
-
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
@@ -10,10 +10,13 @@ class App extends Component {
 
   render(){
     return (
+      <BrowserRouter>
       <div>
-        <Main/>
 
+        <Main/>
+        
       </div>
+      </BrowserRouter>
     );
   }
 
