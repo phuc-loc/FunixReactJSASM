fff3ea4783a762574619f0262cbf1f582f295e83
phuc-loc <phuclocnguyen0701@gmail.com>
2022-01-17 15:02:11 +0700

    SPA páºart 1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -13,44 +13,50 @@ class Header extends Component {
         this.toggleNav = this.toggleNav.bind(this);
     }
 
+
+
     toggleNav () {
         this.setState( {isNavOpen: !this.state.isNavOpen} );
     }
+
+
     render (){
         return (
             <React.Fragment>
                 <Navbar dark expand="md">
                 <div className="container">
+                        
                         <NavbarToggler onClick={ this.toggleNav } />
                         
                         <NavbarBrand className="mr-auto" href="/">
                             <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
                         </NavbarBrand>
-
+                        
+                        
                         <Collapse isOpen={ this.state.isNavOpen } navbar>
                         <Nav navbar>
 
                             <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        <span className="fa fa-home fa-lg"></span>Home
+                                    <NavLink className="nav-link" to="/hom">
+                                        <span className="fa fa-home fa-lg"></span> Home
                                     </NavLink>
                             </NavItem>
                             
                             <NavItem>
                                     <NavLink className="nav-link" to="/aboutus">
-                                        <span className="fa fa-info fa-lg"></span>About Us
+                                        <span className="fa fa-info fa-lg"></span> About Us
                                     </NavLink>
                             </NavItem>
                             
                             <NavItem>
                                     <NavLink className="nav-link" to="/menu">
-                                        <span className="fa fa-list fa-lg"></span>Menu
+                                        <span className="fa fa-list fa-lg"></span> Menu
                                     </NavLink>
                             </NavItem>
 
                             <NavItem>
                                     <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-card fa-lg"></span>Contact Us
+                                        <span className="fa fa-address-card fa-lg"></span> Contact Us
                                     </NavLink>
                             </NavItem>
                         
